d9457edd7ede455d5257dc1789e9c1898f8d856e
Hamza Heikal <hamza.heikal@outlook.com>
2025-11-27 22:07:35 +0200

    add script and update readme

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pacman -S stow chrome exfat-utils pacman blender pacman discord dust yazi
+
+echo  "cloning dotfiles"
+git clone https://github.com/HamzaHeikal2011/dotfiles.git
+
+read -p "Copy dotfiles automaticaly? (y/n): "
+case "$choice" in
+  y|Y )
+    echo "Proceeding..."
+    stow --adopt .
+    ;;
+  n|N )
+    echo "Finishing, Enjoy :)"
+    exit 0 # Exit the script if not confirmed
+
