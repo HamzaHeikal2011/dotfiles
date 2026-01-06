ba8e2e724b9b48e041ad558e511a7359ce16ab3d
Hamza Heikal <hamza.heikal@outlook.com>
2026-01-06 20:59:11 +0200

    map leaders cuz they're laggy

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- map leaders to ensure they're working
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
