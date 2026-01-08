b2ffc8972c78b7290251dbaa39d343184e446a36
Hamza Heikal <hamza.heikal@outlook.com>
2026-01-08 10:59:00 +0200

    update theme to new omarchy format

--- .config/xplr/init.lua
+++
@@ -1,6 +0,0 @@
-version = '1.0.1'
-
-local home = os.getenv 'HOME'
-package.path = home .. '/.config/xplr/plugins/?/init.lua;' .. home .. '/.config/xplr/plugins/?.lua;' .. package.path
-
-require('icons').setup()
