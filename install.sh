c461e4b37deb100b3da9ce06b9f51d7e568712a3
HamzaHeikal2011 <hamza.heikal@outlook.com>
2026-04-18 20:53:07 +0200

    install.sh: more progression (base-devel) + hypr/bindings.conf: fix lagging out terminal (Only graphics issue now)

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #! /bin/zsh
 
-echo 'Installing Hyprland Setup...'
+echo "Congrats: You've passed one of the hardest parts of installing linux. Now to the second part which could either go as smooth as butter or as hard as.... "
+
+echo 'Installing Hyprland Dependencies...'
 sudo pacman -S uwsm hyprsunset hyprlock hypridle waybar swaync swaybg
+echo 'Hyprland dependencies installed.'
+
+echo "Now onto general development stuff..."
+sudo pacman -S base-devel 
